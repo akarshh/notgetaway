b8aa367c09c988b03370b71b0d58cfaa2f4ddf64
Akarsh Hemrajani <ahemrajani3@gatech.edu>
2017-10-21 23:42:34 -0500

    adding vision features

---
+++ labels.py
@@ -0,0 +1,267 @@
+#!/usr/bin/env python
+
+# Copyright 2017 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""This application demonstrates how to perform basic operations with the
+Google Cloud Vision API.
+
+Example Usage:
+python detect.py text ./resources/wakeupcat.jpg
+python detect.py labels ./resources/landmark.jpg
+python detect.py web ./resources/landmark.jpg
+python detect.py web-uri http://wheresgus.com/dog.JPG
+python detect.py faces-uri gs://your-bucket/file.jpg
+
+For more information, the documentation at
+https://cloud.google.com/vision/docs.
+"""
+
+import argparse
+import io
+
+from google.cloud import vision
+from google.cloud.vision import types
+
+# [START def_detect_labels]
+def detect_labels(path):
+    """Detects labels in the file."""
+    client = vision.ImageAnnotatorClient()
+
+    # [START migration_label_detection]
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
+
+    image = types.Image(content=content)
+
+    response = client.label_detection(image=image)
+    labels = response.label_annotations
+    print('Labels:')
+
+    for label in labels:
+        print(label.description)
+    # [END migration_label_detection]
+# [END def_detect_labels]
+
+
+# [START def_detect_labels_uri]
+def detect_labels_uri(uri):
+    """Detects labels in the file located in Google Cloud Storage or on the
+    Web."""
+    client = vision.ImageAnnotatorClient()
+    image = types.Image()
+    image.source.image_uri = uri
+
+    response = client.label_detection(image=image)
+    labels = response.label_annotations
+    print('Labels:')
+
+    for label in labels:
+        print(label.description)
+# [END def_detect_labels_uri]
+
+# [START def_detect_logos]
+def detect_logos(path):
+    """Detects logos in the file."""
+    client = vision.ImageAnnotatorClient()
+
+    # [START migration_logo_detection]
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
+
+    image = types.Image(content=content)
+
+    response = client.logo_detection(image=image)
+    logos = response.logo_annotations
+    print('Logos:')
+
+    for logo in logos:
+        print(logo.description)
+    # [END migration_logo_detection]
+# [END def_detect_logos]
+
+# [START def_detect_logos_uri]
+def detect_logos_uri(uri):
+    """Detects logos in the file located in Google Cloud Storage or on the Web.
+    """
+    client = vision.ImageAnnotatorClient()
+    image = types.Image()
+    image.source.image_uri = uri
+
+    response = client.logo_detection(image=image)
+    logos = response.logo_annotations
+    print('Logos:')
+
+    for logo in logos:
+        print(logo.description)
+# [END def_detect_logos_uri]
+
+# [START def_detect_text]
+def detect_text(path):
+    """Detects text in the file."""
+    client = vision.ImageAnnotatorClient()
+
+    # [START migration_text_detection]
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
+
+    image = types.Image(content=content)
+
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
+    print('Texts:')
+
+    for text in texts:
+        print('\n"{}"'.format(text.description))
+
+        vertices = (['({},{})'.format(vertex.x, vertex.y)
+                    for vertex in text.bounding_poly.vertices])
+
+        print('bounds: {}'.format(','.join(vertices)))
+    # [END migration_text_detection]
+# [END def_detect_text]
+
+
+# [START def_detect_text_uri]
+def detect_text_uri(uri):
+    """Detects text in the file located in Google Cloud Storage or on the Web.
+    """
+    client = vision.ImageAnnotatorClient()
+    image = types.Image()
+    image.source.image_uri = uri
+
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
+    print('Texts:')
+
+    for text in texts:
+        print('\n"{}"'.format(text.description))
+
+        vertices = (['({},{})'.format(vertex.x, vertex.y)
+                    for vertex in text.bounding_poly.vertices])
+
+        print('bounds: {}'.format(','.join(vertices)))
+# [END def_detect_text_uri]
+
+
+# [START def_detect_properties]
+def detect_properties(path):
+    """Detects image properties in the file."""
+    client = vision.ImageAnnotatorClient()
+
+    # [START migration_image_properties]
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
+
+    image = types.Image(content=content)
+
+    response = client.image_properties(image=image)
+    props = response.image_properties_annotation
+    print('Properties:')
+
+    for color in props.dominant_colors.colors:
+        print('fraction: {}'.format(color.pixel_fraction))
+        print('\tr: {}'.format(color.color.red))
+        print('\tg: {}'.format(color.color.green))
+        print('\tb: {}'.format(color.color.blue))
+        print('\ta: {}'.format(color.color.alpha))
+    # [END migration_image_properties]
+# [END def_detect_properties]
+
+
+# [START def_detect_properties_uri]
+def detect_properties_uri(uri):
+    """Detects image properties in the file located in Google Cloud Storage or
+    on the Web."""
+    client = vision.ImageAnnotatorClient()
+    image = types.Image()
+    image.source.image_uri = uri
+
+    response = client.image_properties(image=image)
+    props = response.image_properties_annotation
+    print('Properties:')
+
+    for color in props.dominant_colors.colors:
+        print('frac: {}'.format(color.pixel_fraction))
+        print('\tr: {}'.format(color.color.red))
+        print('\tg: {}'.format(color.color.green))
+        print('\tb: {}'.format(color.color.blue))
+        print('\ta: {}'.format(color.color.alpha))
+# [END def_detect_properties_uri]
+
+def run_local(args):
+    if args.command == 'labels':
+        detect_labels(args.path)
+    elif args.command == 'text':
+        detect_text(args.path)
+    elif args.command == 'logos':
+        detect_logos(args.path)
+    elif args.command == 'properties':
+        detect_properties(args.path)
+
+def run_uri(args):
+    if args.command == 'text-uri':
+        detect_text_uri(args.uri)
+    elif args.command == 'labels-uri':
+        detect_labels_uri(args.uri)
+    elif args.command == 'logos-uri':
+        detect_logos_uri(args.uri)
+    elif args.command == 'properties-uri':
+        detect_properties_uri(args.uri)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    subparsers = parser.add_subparsers(dest='command')
+
+    detect_labels_parser = subparsers.add_parser(
+        'labels', help=detect_labels.__doc__)
+    detect_labels_parser.add_argument('path')
+
+    labels_file_parser = subparsers.add_parser(
+        'labels-uri', help=detect_labels_uri.__doc__)
+    labels_file_parser.add_argument('uri')
+
+    detect_text_parser = subparsers.add_parser(
+        'text', help=detect_text.__doc__)
+    detect_text_parser.add_argument('path')
+
+    text_file_parser = subparsers.add_parser(
+        'text-uri', help=detect_text_uri.__doc__)
+    text_file_parser.add_argument('uri')
+
+    detect_logos_parser = subparsers.add_parser(
+        'logos', help=detect_logos.__doc__)
+    detect_logos_parser.add_argument('path')
+
+    logos_file_parser = subparsers.add_parser(
+        'logos-uri', help=detect_logos_uri.__doc__)
+    logos_file_parser.add_argument('uri')
+
+    properties_parser = subparsers.add_parser(
+        'properties', help=detect_properties.__doc__)
+    properties_parser.add_argument('path')
+
+    properties_file_parser = subparsers.add_parser(
+        'properties-uri',
+        help=detect_properties_uri.__doc__)
+    properties_file_parser.add_argument('uri')
+
+    args = parser.parse_args()
+
+    if ('uri' in args.command):
+        run_uri(args)
+    else:
+        run_local(args)
